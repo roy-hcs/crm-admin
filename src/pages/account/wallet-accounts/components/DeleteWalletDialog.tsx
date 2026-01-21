3fb11281ba4489af876373b121edb6ce9d6f617c
hcs_gsl <gsl@hcs55.com>
2026-01-21 14:35:07 +0800

    feat: CRM4FX-6542-CRM4FX-6539-钱包账户-新增删除完成

---
+++ src/pages/account/wallet-accounts/components/DeleteWalletDialog.tsx
@@ -0,0 +1,78 @@
+import { RrhDialog } from '@/components/common/RrhDialog';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'sonner';
+import { useState } from 'react';
+import { CircleAlert } from 'lucide-react';
+import { RrhButton } from '@/components/common/RrhButton';
+import { useDeleteWallet } from '@/api/hooks/account';
+
+export const DeleteWalletDialog = ({
+  open,
+  setOpen,
+  id,
+  title,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  id: string;
+  title: string;
+}) => {
+  const { t } = useTranslation();
+  const { mutateAsync: deleteWallet } = useDeleteWallet();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = async () => {
+    setIsSubmitting(true);
+    try {
+      const res = await deleteWallet({
+        ids: id,
+      });
+      if (res.code === 0) {
+        toast.success(t('common.success'));
+        setOpen(false);
+      } else {
+        toast.error(res.msg);
+      }
+    } catch {
+      toast.error(t('common.AnErrorOccurred'));
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const onCancel = () => {
+    setOpen(false);
+  };
+
+  return (
+    <RrhDialog
+      title={title}
+      trigger={<button></button>}
+      isConfirmDisabled={isSubmitting}
+      open={open}
+      onOpenChange={setOpen}
+      footerShow={false}
+      variant="small"
+      formLoading={isSubmitting}
+    >
+      <div>
+        <div className="bg-destructive/5 flex items-center gap-1 rounded-md p-2">
+          <CircleAlert className="text-destructive h-4 w-4" />
+          <span className="text-destructive text-sm leading-5 font-medium">
+            {t('walletAccountsPage.deleteTips')}
+          </span>
+        </div>
+        <div className="col-span-full -mx-6 flex justify-end px-6 pt-6 pb-6 sm:pb-0">
+          <div className="flex justify-end gap-4">
+            <RrhButton variant="outline" type="button" className="px-4 py-2" onClick={onCancel}>
+              {t('common.Cancel')}
+            </RrhButton>
+            <RrhButton type="button" className="px-4 py-2" onClick={onSubmit}>
+              {t('common.Confirm')}
+            </RrhButton>
+          </div>
+        </div>
+      </div>
+    </RrhDialog>
+  );
+};
